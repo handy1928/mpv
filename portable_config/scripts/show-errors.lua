00fa2bca7426823081a006b086e88e125902b4af
handy1928 <hendrik@hanbek.de>
2023-09-06 01:20:04 +0200

    disable show-error script

--- portable_config/scripts/show-errors.lua
+++ portable_config/scripts/show-errors.lua
@@ -21,51 +21,55 @@ local o = {
     warnings = false,
 
     --number of seconds to show the OSD messages
-    timeout = 4
+    timeout = 4,
+    --enable/disable script
+    print_message = true,
 }
 
 opt.read_options(o, 'show_errors')
 
---splits the string into a table on the semicolons
-local blacklist = {}
-for str in string.gmatch(o.blacklist, "([^|]+)") do
-    msg.verbose('adding "' .. str .. '" to blacklist')
-    blacklist[str] = true
-end
-
-local ov = mp.create_osd_overlay("ass-events")
-
-if o.warnings then
-    mp.enable_messages('warn')
-else
-    mp.enable_messages('error')
-end
-
-mp.register_event('log-message', function(log)
-    --the log messages seem to always end with a newline character and extra space
-    if blacklist[log.text:sub(1, -3)] or blacklist[log.prefix] then return end
-    local colour = ""
-
-    if log.level == "error" then
-        colour = "{\\c&H0000AA>&}"
-    elseif log.level == "warn" then
-        colour = "{\\c&H1AA3FF>&}"
-    elseif log.level == "fatal" then
-        colour = "{\\c&H1A75FF>&}"
+if o.print_message then
+    --splits the string into a table on the semicolons
+    local blacklist = {}
+    for str in string.gmatch(o.blacklist, "([^|]+)") do
+        msg.verbose('adding "' .. str .. '" to blacklist')
+        blacklist[str] = true
     end
 
-    message = colour .. "[" .. log.prefix .. "] " .. log.text
-    ov.data = ov.data .. message
-    ov:update()
+    local ov = mp.create_osd_overlay("ass-events")
 
-    mp.add_timeout(o.timeout, function ()
-        local endln = ov.data:find('\n') + 1
-        ov.data = ov.data:sub(endln)
+    if o.warnings then
+        mp.enable_messages('warn')
+    else
+        mp.enable_messages('error')
+    end
+
+    mp.register_event('log-message', function(log)
+        --the log messages seem to always end with a newline character and extra space
+        if blacklist[log.text:sub(1, -3)] or blacklist[log.prefix] then return end
+        local colour = ""
+
+        if log.level == "error" then
+            colour = "{\\c&H0000AA>&}"
+        elseif log.level == "warn" then
+            colour = "{\\c&H1AA3FF>&}"
+        elseif log.level == "fatal" then
+            colour = "{\\c&H1A75FF>&}"
+        end
+
+        message = colour .. "[" .. log.prefix .. "] " .. log.text
+        ov.data = ov.data .. message
+        ov:update()
+
+        mp.add_timeout(o.timeout, function ()
+            local endln = ov.data:find('\n') + 1
+            ov.data = ov.data:sub(endln)
+            ov:update()
+        end)
+    end)
+
+    mp.register_script_message('clear-errors', function()
+        ov.data = ""
         ov:update()
     end)
-end)
-
-mp.register_script_message('clear-errors', function()
-    ov.data = ""
-    ov:update()
-end)
+end
